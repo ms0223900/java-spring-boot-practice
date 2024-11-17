09a746fe1de96509a591cb9836516a3fbc5f4e57
penguin-cho <penguin-cho@cloud-latitude.com>
2024-11-17 21:46:16 +0800

    test: add more tests

--- complete/src/test/java/com/leetcode/ShortestSubarrayTest.java
+++ complete/src/test/java/com/leetcode/ShortestSubarrayTest.java
@@ -21,4 +21,16 @@ public class ShortestSubarrayTest {
         ShortestSubarray shortestSubarray = new ShortestSubarray();
         assertEquals(3, shortestSubarray.shortestSubarray(new int[] { 2, -1, 2 }, 3));
     }
+
+    @Test
+    public void testShortestSubarray4() {
+        ShortestSubarray shortestSubarray = new ShortestSubarray();
+        assertEquals(2, shortestSubarray.shortestSubarray(new int[] { 17, 85, 93, -45, -21 }, 150));
+    }
+
+    @Test
+    public void testShortestSubarray5() {
+        ShortestSubarray shortestSubarray = new ShortestSubarray();
+        assertEquals(1, shortestSubarray.shortestSubarray(new int[] { -1, -1, 3, -1, -1 }, 2));
+    }
 }
