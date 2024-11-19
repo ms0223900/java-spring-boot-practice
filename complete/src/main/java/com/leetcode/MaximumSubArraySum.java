a1e9c9af19d0d13bfc146e8fbe01814e8f992169
penguin-cho <penguin-cho@cloud-latitude.com>
2024-11-19 21:45:24 +0800

    feat: add another solution

--- complete/src/main/java/com/leetcode/MaximumSubArraySum.java
+++ complete/src/main/java/com/leetcode/MaximumSubArraySum.java
@@ -1,12 +1,29 @@
 package com.leetcode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class MaximumSubArraySum {
     public long maximumSubarraySum(int[] nums, int k) {
-        int maxSum = 0;
+        long maxSum = 0;
+        long currentSum = 0;
+
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length - k + 1; i++) {
-            int sum = 0;
+            long sum = 0;
             int preNum = -1;
+
+            if (currentSum > 0 && !set.contains(nums[i + k - 1])) {
+                sum = currentSum - nums[i - 1] + nums[i + k - 1];
+                maxSum = Math.max(maxSum, sum);
+                currentSum = sum;
+                set.add(nums[i + k - 1]);
+                continue;
+            }
             for (int j = i; j < i + k; j++) {
+                set.add(nums[j]);
                 if (preNum == nums[j]) {
                     sum = 0;
                     break;
@@ -14,9 +31,36 @@ public class MaximumSubArraySum {
                 sum += nums[j];
                 preNum = nums[j];
             }
+            currentSum = sum;
             maxSum = Math.max(maxSum, sum);
         }
         return maxSum;
     }
 
+    public long maximumSubarraySum2(int[] nums, int k) {
+        long maxSum = 0;
+        long currentSum = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            currentSum += nums[right];
+
+            int index = map.getOrDefault(nums[right], -1);
+
+            while (left <= index || (right - left + 1) > k) {
+                currentSum -= nums[left];
+                left++;
+            }
+
+            if ((right - left + 1) == k) {
+                maxSum = Math.max(maxSum, currentSum);
+            }
+
+            map.put(nums[right], right);
+        }
+
+        return maxSum;
+    }
 }
--- complete/src/test/java/com/leetcode/MaximumSubArraySumTest.java
+++ complete/src/test/java/com/leetcode/MaximumSubArraySumTest.java
@@ -8,11 +8,34 @@ public class MaximumSubArraySumTest {
     public void testMaximumSubArraySum() {
         MaximumSubArraySum maximumSubArraySum = new MaximumSubArraySum();
         assertEquals(15, maximumSubArraySum.maximumSubarraySum(new int[] { 1, 5, 4, 2, 9, 9, 9 }, 3));
+        assertEquals(15, maximumSubArraySum.maximumSubarraySum2(new int[] { 1, 5, 4, 2, 9, 9, 9 }, 3));
+    }
+
+    @Test
+    public void testMaximumSubArraySum1() {
+        MaximumSubArraySum maximumSubArraySum = new MaximumSubArraySum();
+        assertEquals(16, maximumSubArraySum.maximumSubarraySum(new int[] { 2, 5, 4, 5 }, 4));
+        assertEquals(16, maximumSubArraySum.maximumSubarraySum2(new int[] { 2, 5, 4, 5 }, 4));
     }
 
     @Test
     public void testMaximumSubArraySum2() {
         MaximumSubArraySum maximumSubArraySum = new MaximumSubArraySum();
         assertEquals(0, maximumSubArraySum.maximumSubarraySum(new int[] { 4, 4, 4 }, 3));
+        assertEquals(0, maximumSubArraySum.maximumSubarraySum2(new int[] { 4, 4, 4 }, 3));
+    }
+
+    @Test
+    public void testMaximumSubArraySum3() {
+        MaximumSubArraySum maximumSubArraySum = new MaximumSubArraySum();
+        assertEquals(24, maximumSubArraySum.maximumSubarraySum(new int[] { 1, 1, 1, 7, 8, 9 }, 3));
+        assertEquals(24, maximumSubArraySum.maximumSubarraySum2(new int[] { 1, 1, 1, 7, 8, 9 }, 3));
+    }
+
+    @Test
+    public void testMaximumSubArraySum4() {
+        MaximumSubArraySum maximumSubArraySum = new MaximumSubArraySum();
+        assertEquals(12, maximumSubArraySum.maximumSubarraySum(new int[] { 9, 9, 9, 1, 2, 3 }, 3));
+        assertEquals(12, maximumSubArraySum.maximumSubarraySum2(new int[] { 9, 9, 9, 1, 2, 3 }, 3));
     }
 }
